d39bac96fb8f0d1824b7137105499eab47c74f88
lisatondwal <lisatondwal3792@gmail.com>
2022-12-02 10:21:41 +0530

    demoblaze changes
    hii ...i do some changes

---
+++ DemoBlazedemo/src/main/java/DemoBlazedemo/DemoBlazedemo/App.java
@@ -0,0 +1,13 @@
+package DemoBlazedemo.DemoBlazedemo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
